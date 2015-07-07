3c300959f249d785f793ca512b5f0d5f8b981153
zhouyu <zhouyu@liulishuo.com>
2015-07-07 12:00:57 +0800

    Initial Commit

---
+++ CYSystemVersion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CYSystemVersion
+//
+//  Created by YuriJou on 7/7/15.
+//  Copyright (c) 2015 YuriJou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
